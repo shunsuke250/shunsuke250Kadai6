f44aa2a22e23096f9afbdcf541dd0930da6d34fd
副山俊輔 <soeyamashunsuke@MacBook-Pro.local>
2023-09-09 18:23:39 +0900

    :sparkles: プロジェクトを作成

---
+++ Slider23-09-09/Slider23-09-09/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Slider23-09-09
+//
+//  Created by 副山俊輔 on 2023/09/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
