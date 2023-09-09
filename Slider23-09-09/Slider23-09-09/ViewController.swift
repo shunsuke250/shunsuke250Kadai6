564bf4f6c790280c91cc88a9455b982047335307
副山俊輔 <soeyamashunsuke@MacBook-Pro.local>
2023-09-09 20:38:17 +0900

    :shirt: 不要なスペースを削除

--- Slider23-09-09/Slider23-09-09/ViewController.swift
+++ Slider23-09-09/Slider23-09-09/ViewController.swift
@@ -10,14 +10,14 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet private weak var questionNumLabel: UILabel!
     @IBOutlet private weak var answerSlider: UISlider!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         questionNumLabel.text = "\(questionValue)"
     }
-    
+
     private lazy var questionValue = randomValue
-    
+
     private var randomValue: Int {
         return Int.random(in: 1...100)
     }
@@ -38,10 +38,9 @@ class ViewController: UIViewController {
         let okButton = UIAlertAction(title: "OK", style: .default) { (_) in
             self.dismiss(animated: true, completion: nil)
         }
-        
+
         alert.addAction(okButton)
         present(alert, animated: true, completion: nil)
     }
-    
-}
 
+}
