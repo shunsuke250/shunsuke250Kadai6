b73700490a8d5260795461e0e5dcdcf52a9539ef
副山俊輔 <soeyamashunsuke@MacBook-Pro.local>
2023-09-09 18:37:25 +0900

    :sparkles: UI部品を追加しViewControllerと紐づけ

--- Slider23-09-09/Slider23-09-09/ViewController.swift
+++ Slider23-09-09/Slider23-09-09/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet private weak var questionNumLabel: UILabel!
+    @IBOutlet private weak var answerSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction private func didTapJudgeButton(_ sender: UIButton) {
+    }
+    
 }
 
